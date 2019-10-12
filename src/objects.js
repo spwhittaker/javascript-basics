125b86f35aaa7ddd7a15f75342c1114ad545c722
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-12 15:14:15 +0100

    all objects tests passed

--- src/objects.js
+++ src/objects.js
@@ -1,5 +1,8 @@
 const createPerson = (name, age) => {
-  return { name: name, age: age };
+  return {
+    name: name,
+    age: age
+  };
 };
 
 const getName = object => {
@@ -12,36 +15,65 @@ const getProperty = (property, object) => {
 
 const hasProperty = (property, object) => {
   if (object[property]) {
-    return true;}
-    else {return false;}
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isOver65 = person => {
-  if (person["age"] > 65) {return true;}
+  if (person["age"] > 65) {
+    return true;
+  }
   return false;
 };
 
 const getAges = people => {
   let ages = [];
-people.forEach(function (person) {ages.push(person["age"])});
-return ages;
+  people.forEach(function (person) {
+    ages.push(person["age"])
+  });
+  return ages;
 };
 
-const findByName = (name, people) => {
+const findByName = (findName, people) => {
+  for (let i = 0; i < people.length; i++) {
+    if (people[i]["name"] === findName) {
+      return people[i];
+    }
 
+  }
 };
 
 const findHondas = cars => {
-  // your code here
+  let resultArray = []
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].manufacturer === "Honda") {
+      resultArray.push(cars[i]);
+    }
+  }
+  return resultArray;
 };
 
 const averageAge = people => {
-  // your code here
+  let ageArray = [];
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  for (let i = 0; i < people.length; i++) {
+    ageArray.push(people[i]["age"]);
+  }
+  let totalAges = ageArray.reduce(reducer);
+  let averAge = totalAges / ageArray.length;
+  return averAge;
 };
 
-const createTalkingPerson = (name, age) => {
-  // your code here
-};
+const createTalkingPerson = function (personName, age) {
+    const talkingPerson = {"name": personName,
+    "age" : age,
+    "introduce": function(greetedPerson) {
+      return `Hi ${greetedPerson}, my name is ${talkingPerson["name"]} and I am ${talkingPerson["age"]}!`;
+    }}
+    return talkingPerson;
+  };
 
 module.exports = {
   createPerson,
